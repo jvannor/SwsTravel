25e27b33c04e2136da98beaf803df8ca86f766a4
Jeff Van Nortwick <jeffvan@microsoft.com>
2022-05-10 10:07:43 -0400

    retry timeout

--- api/Program.cs
+++ api/Program.cs
@@ -8,13 +8,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
 using api.entities;
 
-// https://medium.com/@manuelspinto/create-a-complete-azure-function-project-in-net-6-and-af-v4-bd1cc714452c
-// https://github.com/manuelspinto/azure-function-example-csharp
-// https://dev.to/azure/using-entity-framework-with-azure-functions-50aa
-// https://medium.com/geekculture/developing-net-isolated-process-azure-functions-5a1ff4acee46
-// https://github.com/Azure/Azure-Functions/issues/717#:~:text=The%20connection%20string%20in%20the%20local.settings.json%20does%20indeed,name%20%29%20%7B%20string%20conStr%20%3D%20System.%20
-// https://github.com/jeffhollan/functions-net5-entityframework/blob/main/HttpTrigger.cs
-
 namespace api
 {
     public class Program
@@ -33,7 +26,7 @@ namespace api
                                 {
                                     sqlOptions.EnableRetryOnFailure(
                                         maxRetryCount: 10,
-                                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                                        maxRetryDelay: TimeSpan.FromSeconds(120),
                                         errorNumbersToAdd: null);
                                 }
                             )
